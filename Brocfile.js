bc787f3940f1ab8a5b2c006b4a95070d4b7edf84
Will Strinz <wstrinz@gmail.com>
2014-07-21 15:42:05 -0500

    add bootstrap

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,8 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('vendor/bootstrap/dist/js/bootstrap.js');
+app.import('vendor/bootstrap/dist/css/bootstrap.css');
+app.import('vendor/bootstrap/dist/css/bootstrap-theme.css');
+
 module.exports = app.toTree();
