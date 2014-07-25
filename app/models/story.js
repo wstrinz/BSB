b224cda1e20e81c68f3979733200f838a1bb9b75
Will Strinz <wstrinz@gmail.com>
2014-07-25 16:20:50 -0500

    add published field, not working yet

--- app/models/story.js
+++ app/models/story.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   author: DS.attr('string'),
   url: DS.attr('string'),
+  published: DS.attr('date'),
   summary: DS.attr('string'),
   story_content: DS.attr('string'),
   feed: DS.belongsTo('feed')
