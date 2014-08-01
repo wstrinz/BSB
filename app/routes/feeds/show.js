8fab88b8e20f7715119d8333ad14d11ae09b147c
Will Strinz <wstrinz@gmail.com>
2014-08-01 12:27:38 -0500

    somewhat working sorting

--- app/routes/feeds/show.js
+++ app/routes/feeds/show.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params){
-    return this.store.find('feed', params.feed_id);
+    return this.store.find('feed', params.feed_id).then(
+      function(f){ return f.get('stories'); }
+    );
   }
 });
