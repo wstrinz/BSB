5c14350aa2a7d077b736e9b2e05d3e0a7ce196c8
Will Strinz <wstrinz@gmail.com>
2014-12-11 16:27:31 -0600

    use archive in special HN feed

--- lib/models/feeds/hacker_news.rb
+++ lib/models/feeds/hacker_news.rb
@@ -1,9 +1,13 @@
 require 'ruby-hackernews'
 class HackerNewsFeed < Feed
+  def in_archive?(entry)
+    StoryArchive.contains?(entry.link.href) || StoryArchive.contains?("http://news.ycombinator.com/#{s.url}")
+  end
+
   def update
     entries = RubyHackernews::Entry.all
     t = Time.now
-    news = entries.select{ |e| Story.where(url: e.link.href).empty? }
+    news = entries.select{ |e| Story.where(url: e.link.href).empty? && !in_archive(entry) }
     logger.info "Hacker News added #{news.count} stories"
     news.each do |e|
       s = Story.new
